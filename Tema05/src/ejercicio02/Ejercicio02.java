715b31bff8581d1eb929f561b4957c83e2242291
mreqmer <145675425+mreqmer@users.noreply.github.com>
2024-03-06 07:19:13 +0100

    2-3

--- Tema05/src/ejercicio02/Ejercicio02.java
+++ Tema05/src/ejercicio02/Ejercicio02.java
@@ -3,8 +3,20 @@ package ejercicio02;
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		Libro lb = new Libro("Mistborn", "a", 12, 2);
-		System.out.println();
+		Libro lb1 = new Libro("Mistborn", "Brandon Sanderson", 12, 2);
+		
+		System.out.println(lb1);
+		
+		Libro lb2;
+		
+		lb2 = new Libro("libro", "a", 1, 2 );
+		
+		System.out.println(lb2);
+		
+		
+		lb2.setAutor("patata");
+		System.out.println(lb2.getAutor());
+		
 	}
 
 }
--- Tema05/src/ejercicio02/Libro.java
+++ Tema05/src/ejercicio02/Libro.java
@@ -12,18 +12,18 @@ public class Libro {
 	}
 
 	public Libro(String titulo, String autor, int ejemplares, int prestados) {
-		if (this.titulo != null) {
+		if (titulo != null) {
 			this.titulo = titulo;
 		}
-		if (this.autor != null && !this.autor.equals("")) {
+		if (autor != null && !autor.equals("")) {
 			this.autor = autor;
 		}
 
-		if (this.ejemplares >= 0) {
+		if (ejemplares >= 0) {
 			this.ejemplares = ejemplares;
 		}
 
-		if (this.prestados >= 0) {
+		if (prestados >= 0) {
 			this.prestados = prestados;
 		}
 
@@ -51,4 +51,50 @@ public class Libro {
 		return devolucion;
 	}
 
+	public String toString() {
+
+		String cadena = "";
+
+		cadena += this.titulo + " ";
+		cadena += this.autor + " ";
+		cadena += this.ejemplares + " ";
+		cadena += this.prestados + " ";
+
+		return cadena;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getEjemplares() {
+		return ejemplares;
+	}
+
+	public void setEjemplares(int ejemplares) {
+		this.ejemplares = ejemplares;
+	}
+
+	public int getPrestados() {
+		return prestados;
+	}
+
+	public void setPrestados(int prestados) {
+		this.prestados = prestados;
+	}
+
+	
+	
 }
