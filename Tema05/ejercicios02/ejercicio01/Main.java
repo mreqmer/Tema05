30607df8f261b7c2b7b6edc36a07b10d7d28df21
mreqmer <145675425+mreqmer@users.noreply.github.com>
2024-03-12 23:17:20 +0100

    12-03

--- Tema05/ejercicios02/ejercicio01/Main.java
+++ Tema05/ejercicios02/ejercicio01/Main.java
@@ -22,7 +22,7 @@ public class Main {
 		System.out.println("Introduce la opción del menú: ");
 		opc = sc.nextInt();
 
-		while(true) {
+		while (true) {
 			switch (opc) {
 			case 1 -> {
 				opc1(alumnos);
@@ -30,9 +30,20 @@ public class Main {
 			case 2 -> {
 				System.out.println("Nombre del alumno: ");
 				nombre = sc.next();
-				System.out.println("Nota Media del aLumno: ");
+				System.out.println("Nota media del alumno: ");
 				notaMedia = sc.nextInt();
 				opc2(nombre, notaMedia, alumnos);
+			}
+			case 3 -> {
+				System.out.println("Nombre del alumno: ");
+				nombre = sc.next();
+				System.out.println("Nueva nota media: ");
+				notaMedia = sc.nextInt();
+				opc3(nombre, notaMedia, alumnos);
+			}case 4 -> {
+				System.out.println("Nombre del alumno: ");
+				nombre = sc.next();
+				opc4(nombre, alumnos);
 			}
 
 			}
@@ -66,7 +77,7 @@ public class Main {
 	 * 
 	 * @param arrayAlumnos Tabla con los objetos Alumno
 	 */
-	static public void opc1(Alumno[] arrayAlumnos) {
+	public static void opc1(Alumno[] arrayAlumnos) {
 
 		for (int i = 0; i < arrayAlumnos.length; i++) {
 			if (arrayAlumnos[i] != null) {
@@ -75,15 +86,59 @@ public class Main {
 		}
 	}
 
-	static public void opc2(String nombreA, double notaMediaA, Alumno[] tablaAlumnos) {
+	/**
+	 * Crea nuevos alumnos en un array
+	 * 
+	 * @param nombreA      Nombre que tendrá el objeto Alumno
+	 * @param notaMediaA   Nota media que tendrá el objeto aAlumno
+	 * @param tablaAlumnos Array donde se meterán los objetos Alumno
+	 */
+	public static void opc2(String nombreA, double notaMediaA, Alumno[] tablaAlumnos) {
 
 		for (int i = 0; i < tablaAlumnos.length; i++) {
+
 			if (tablaAlumnos[i] == null) {
 				tablaAlumnos[i] = new Alumno(nombreA, notaMediaA);
 				break;
 			}
 		}
 
+	}
+
+	/**
+	 * Modifica las notas de los alumnos
+	 * 
+	 * @param nombreA      Nombre del alumno al que se le va a modificar la nota
+	 * @param nuevaNota    Nota media nueva que se le va a asignar al alumnos
+	 * @param tablaAlumnos Array donde se guardan los alumnos
+	 */
+	public static void opc3(String nombreA, int nuevaNota, Alumno[] tablaAlumnos) {
+
+		for (int i = 0; i < tablaAlumnos.length; i++) {
+
+			if ( tablaAlumnos[i] != null && tablaAlumnos[i].getNombre().equals(nombreA) ) {
+				tablaAlumnos[i].setNotaMedia(nuevaNota);
+			}
+
+		}
+
+	}
+	
+	/**
+	 * 
+	 * @param nombreA
+	 * @param tablaAlumnos
+	 */
+	public static void opc4(String nombreA, Alumno[] tablaAlumnos) {
+		
+		for (int i = 0; i < tablaAlumnos.length; i++) {
+			
+			if(tablaAlumnos[i] != null && tablaAlumnos[i].getNombre().equals(nombreA)) {
+				tablaAlumnos[i] = null;
+			}
+			
+		}
+		
 	}
 
 }
